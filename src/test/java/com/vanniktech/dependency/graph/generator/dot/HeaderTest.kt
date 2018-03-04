95be2de97650fd2614a18c042405ddf500d971aa
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-03-04 23:01:43 +0100

    Fail early and give tailored message when dot command is not present. (#2)

--- src/test/java/com/vanniktech/dependency/graph/generator/dot/HeaderTest.kt
+++ src/test/java/com/vanniktech/dependency/graph/generator/dot/HeaderTest.kt
@@ -7,6 +7,10 @@ import org.junit.Test
 
 class HeaderTest {
   @Test fun syntax() {
+    assertThat(Header("foo")).hasToString("label=\"foo\" fontsize=\"24\" height=\"5\" labelloc=\"t\" labeljust=\"c\"")
+    assertThat(Header("foo", 10)).hasToString("label=\"foo\" fontsize=\"10\" height=\"5\" labelloc=\"t\" labeljust=\"c\"")
+    assertThat(Header("foo", 10, 25)).hasToString("label=\"foo\" fontsize=\"10\" height=\"25\" labelloc=\"t\" labeljust=\"c\"")
+    assertThat(Header("foo", 10, 25, BOTTOM)).hasToString("label=\"foo\" fontsize=\"10\" height=\"25\" labelloc=\"b\" labeljust=\"c\"")
     assertThat(Header("1234", 100, 5, BOTTOM, LEFT)).hasToString("label=\"1234\" fontsize=\"100\" height=\"5\" labelloc=\"b\" labeljust=\"l\"")
   }
 }
