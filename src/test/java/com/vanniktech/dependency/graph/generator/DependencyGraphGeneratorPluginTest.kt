e76aaef4be2f4227389382b433d7517421f00260
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-03-10 15:56:52 +0100

    Fix Gradle task on Linux and add integration test. (#14)

--- src/test/java/com/vanniktech/dependency/graph/generator/DependencyGraphGeneratorPluginTest.kt
+++ src/test/java/com/vanniktech/dependency/graph/generator/DependencyGraphGeneratorPluginTest.kt
@@ -5,11 +5,17 @@ import org.assertj.core.api.Java6Assertions.assertThat
 import org.gradle.api.internal.project.DefaultProject
 import org.gradle.api.plugins.JavaLibraryPlugin
 import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.GradleRunner
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 import java.io.File
+import java.io.StringWriter
 
 class DependencyGraphGeneratorPluginTest {
+  @get:Rule val testProjectDir = TemporaryFolder()
+
   private lateinit var singleProject: DefaultProject
 
   @Before fun setUp() {
@@ -33,5 +39,62 @@ class DependencyGraphGeneratorPluginTest {
     assertThat(task.inputFile).hasToString(singleProject.buildFile.toString())
     assertThat(task.outputFileDot).hasToString(File(singleProject.buildDir, "dependency-graph.dot").toString())
     assertThat(task.outputFileImage).hasToString(File(singleProject.projectDir, "dependency-graph.png").toString())
+  }
+
+  @Test fun integrationTest() {
+    val classpathString = javaClass.classLoader.getResource("plugin-classpath.txt")
+        .readText()
+        .split(Regex("\\s*\\r?\\n\\s*"))
+        .map { File(it).absolutePath.replace("\\", "\\\\") }
+        .joinToString(", ") { "'$it'" }
+
+    val buildFile = testProjectDir.newFile("build.gradle")
+    buildFile.writeText("""
+        |buildscript {
+        |  dependencies {
+        |    classpath files($classpathString)
+        |  }
+        |}
+        |
+        |apply plugin: "java-library"
+        |apply plugin: "com.vanniktech.dependency.graph.generator"
+        |
+        |repositories {
+        |  mavenCentral()
+        |}
+        |
+        |dependencies {
+        |  api "org.jetbrains.kotlin:kotlin-stdlib:1.2.30"
+        |  implementation "io.reactivex.rxjava2:rxjava:2.1.10"
+        |}""".trimMargin())
+
+    val stdErrorWriter = StringWriter()
+
+    GradleRunner.create()
+        .withPluginClasspath()
+        .withGradleVersion("4.6")
+        .withProjectDir(testProjectDir.root)
+        .withArguments("generateDependencyGraph")
+        .forwardStdError(stdErrorWriter)
+        .build()
+
+    // No errors.
+    assertThat(stdErrorWriter).hasToString("")
+
+    // We don't want to assert the content of the image, just that it exists.
+    assertThat(File(testProjectDir.root, "dependency-graph.png")).exists()
+
+    assertThat(File(testProjectDir.root, "build/dependency-graph.dot")).hasContent("""
+        |digraph G {
+        |  ${testProjectDir.root.name} [label="${testProjectDir.root.name}", shape="box"];
+        |  orgjetbrainskotlinkotlinstdlib [label="kotlin-stdlib", shape="box"];
+        |  ${testProjectDir.root.name} -> orgjetbrainskotlinkotlinstdlib;
+        |  orgjetbrainsannotations [label="annotations", shape="box"];
+        |  orgjetbrainskotlinkotlinstdlib -> orgjetbrainsannotations;
+        |  ioreactivexrxjava2rxjava [label="rxjava", shape="box"];
+        |  ${testProjectDir.root.name} -> ioreactivexrxjava2rxjava;
+        |  orgreactivestreamsreactivestreams [label="reactive-streams", shape="box"];
+        |  ioreactivexrxjava2rxjava -> orgreactivestreamsreactivestreams;
+        |}""".trimMargin())
   }
 }
