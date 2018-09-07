49f20acefe3a6dc30b50f5618c28f68081a7e30f
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-09-07 14:28:44 +0200

    Leverage Task Configuration Avoidance starting with Gradle 4.9 (#72)

--- src/main/kotlin/com/vanniktech/dependency/graph/generator/DependencyGraphGeneratorPlugin.kt
+++ src/main/kotlin/com/vanniktech/dependency/graph/generator/DependencyGraphGeneratorPlugin.kt
@@ -2,21 +2,35 @@ package com.vanniktech.dependency.graph.generator
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.util.GradleVersion
 import java.io.File
 
 open class DependencyGraphGeneratorPlugin : Plugin<Project> {
   override fun apply(project: Project) {
     val extension = project.extensions.create("dependencyGraphGenerator", DependencyGraphGeneratorExtension::class.java)
 
-    project.afterEvaluate {
+    if (GradleVersion.version(project.gradle.gradleVersion) >= GradleVersion.version("4.9")) {
       extension.generators.forEach {
-        val task = project.tasks.create(it.gradleTaskName, DependencyGraphGeneratorTask::class.java)
-        task.generator = it
-        task.group = "reporting"
-        task.description = "Generates a dependency graph${it.name.nonEmptyPrepend(" for ")}"
-        task.inputFile = project.buildFile
-        task.outputDirectory = File(project.buildDir, "reports/dependency-graph/")
+        project.tasks.register(it.gradleTaskName, DependencyGraphGeneratorTask::class.java, it.configureTask(project))
       }
+    } else {
+      project.afterEvaluate { _ ->
+        extension.generators.forEach {
+          project.tasks.create(it.gradleTaskName, DependencyGraphGeneratorTask::class.java, it.configureTask(project))
+        }
+      }
+    }
+  }
+
+  private fun DependencyGraphGeneratorExtension.Generator.configureTask(project: Project): (DependencyGraphGeneratorTask) -> Unit {
+    val name = name.nonEmptyPrepend(" for ")
+
+    return {
+      it.generator = this
+      it.group = "reporting"
+      it.description = "Generates a dependency graph$name"
+      it.inputFile = project.buildFile
+      it.outputDirectory = File(project.buildDir, "reports/dependency-graph/")
     }
   }
 }
