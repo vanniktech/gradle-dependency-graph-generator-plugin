8e3afc6f84dc24e148a3bee3c9b6433360f3bffa
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-10-01 11:52:28 +0200

    Make DependencyGraphGeneratorTask ProjectDependencyGraphGeneratorTask cacheable tasks.

--- src/main/kotlin/com/vanniktech/dependency/graph/generator/DependencyGraphGeneratorTask.kt
+++ src/main/kotlin/com/vanniktech/dependency/graph/generator/DependencyGraphGeneratorTask.kt
@@ -3,12 +3,13 @@ package com.vanniktech.dependency.graph.generator
 import com.vanniktech.dependency.graph.generator.DependencyGraphGeneratorExtension.Generator
 import guru.nidi.graphviz.engine.Graphviz
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
-open class DependencyGraphGeneratorTask : DefaultTask() {
+@CacheableTask open class DependencyGraphGeneratorTask : DefaultTask() {
   lateinit var generator: Generator // TODO does this need to be an input? Quick testing shows no.
   @InputFile lateinit var inputFile: File
 
