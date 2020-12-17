08bef4afdbe0abdd2efa104eb0a148165f5fc277
Trevor Jones <trevorjones141@gmail.com>
2020-12-16 17:01:58 -0700

    mark task inputs as such.

--- src/main/kotlin/com/vanniktech/dependency/graph/generator/DependencyGraphGeneratorTask.kt
+++ src/main/kotlin/com/vanniktech/dependency/graph/generator/DependencyGraphGeneratorTask.kt
@@ -4,17 +4,26 @@ import com.vanniktech.dependency.graph.generator.DependencyGraphGeneratorExtensi
 import guru.nidi.graphviz.engine.Graphviz
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
 @CacheableTask open class DependencyGraphGeneratorTask : DefaultTask() {
-  lateinit var generator: Generator // TODO does this need to be an input? Quick testing shows no.
+  @get:Nested lateinit var generator: Generator
 
   @OutputDirectory lateinit var outputDirectory: File
 
+  val graph by lazy {
+    DependencyGraphGenerator(project, generator).generateGraph()
+  }
+
+  @get:Input val dotFormatGraph by lazy {
+    graph.toString()
+  }
+
   @TaskAction fun run() {
-    val graph = DependencyGraphGenerator(project, generator).generateGraph()
     File(outputDirectory, generator.outputFileNameDot).writeText(graph.toString())
 
     val graphviz = Graphviz.fromGraph(graph)
